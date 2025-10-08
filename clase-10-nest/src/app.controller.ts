9e438d1b0c3b842ac76c2fdb765fc32df8a45048
MaximoTrejo <maximotrejo123@gmail.com>
2025-10-08 19:34:05 -0300

    primer vistazo a nest

--- clase-10-nest/src/app.controller.ts
+++ clase-10-nest/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  //POST-> localhost:3000/a/b/c
+  @Post('a/b/c')
+  crearAlgo(): string {
+    return 'algo'
   }
 }
--- clase-10-nest/src/main.ts
+++ clase-10-nest/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //definir mas configuraciones a nivel app
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
