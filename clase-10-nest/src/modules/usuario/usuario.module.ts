d095eb8d4b8dc465679d7989c23c50062e5dec4e
MaximoTrejo <maximotrejo123@gmail.com>
2025-10-08 21:51:06 -0300

    aaa

--- clase-10-nest/src/modules/usuario/usuario.module.ts
+++ clase-10-nest/src/modules/usuario/usuario.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsuariosController } from './usuarios.controller';
 import { UsuarioService } from './usuario.service';
 
+//controlar que siembre sean rutas relativas 
+
 @Module({
 
     controllers: [UsuariosController],
