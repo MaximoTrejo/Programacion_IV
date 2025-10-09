83cab93e9891cfc5eba3b08ca289aa20c9c90909
MaximoTrejo <maximotrejo123@gmail.com>
2025-10-08 21:53:43 -0300

    sdsdsds

--- clase-10-nest/src/app.module.ts
+++ clase-10-nest/src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosController } from './modules/usuario/usuarios.controller';
 import { RopaController } from './controller/ropa/ropa.controller';
-import { UsuarioModule } from './modules/usuario/usuario.module';
 import { LibroModule } from './resources/libro/libro.module';
 
 @Module({
-  imports: [UsuarioModule, LibroModule],
+  imports: [ LibroModule],
   controllers: [AppController, UsuariosController, RopaController],
   providers: [AppService],
   exports: [],
